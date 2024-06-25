b40c9fa06f21acff2d36818919d3a858dc6fe1eb
konfle <konrad.flejter@gmail.com>
2024-06-25 10:06:29 +0200

    Set default values for optional config variables and error handle for required ones.

--- main.py
+++ main.py
@@ -24,9 +24,15 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
 CHANNEL = int(os.getenv("DISCORD_CHANNEL_ID"))
-SYMBOL = os.getenv("SYMBOL")
-INTERVAL = int(os.getenv("INTERVAL"))
-RSI_PERIOD = int(os.getenv("RSI_PERIOD"))
+
+if TOKEN is None or GUILD is None or CHANNEL is None:
+    logger.error("DISCORD_TOKEN, DISCORD_GUILD, and DISCORD_CHANNEL_ID must be set in the .env file.")
+    exit(1)
+
+# Set default values for SYMBOL, INTERVAL, and RSI_PERIOD
+SYMBOL = os.getenv("SYMBOL", "SOLUSDT")
+INTERVAL = int(os.getenv("INTERVAL", 60))  # It's counted in minutes
+RSI_PERIOD = int(os.getenv("RSI_PERIOD", 14))
 
 # Set up the intents for the bot, specifically requesting permission to access member information
 intents = discord.Intents.default()
